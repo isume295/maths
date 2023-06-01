4957688c52f2854c72ef6163bf54444bc57f4a3a
Sumeya <sumeyaibrahim72@gmail.com>
2023-06-01 22:46:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/calculator';
+import Quote from './components/quote';
 
 function App() {
   return (
-    <div>
+    <div className="app-container">
       <Calculator />
+      <Quote />
     </div>
   );
 }
