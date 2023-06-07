40ec8f3db131f567189a555f94abefd9f38311c0
Denio Nhanale <dnhanale@gmail.com>
2023-06-07 16:25:49 +0200

    Add calculator component test

--- src/test/component.test.js
+++ src/test/component.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Button from '../components/button';
+import Calculator from '../components/calculator';
 
 describe('test components renders correctly', () => {
   test('test 1', () => {
     const { container } = render(<Button value="+" className="column" handleClick={() => {}} />);
     expect(container.firstChild).toMatchSnapshot();
   });
+  test('test 1', () => {
+    const { container } = render(<Calculator />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
 });
+
